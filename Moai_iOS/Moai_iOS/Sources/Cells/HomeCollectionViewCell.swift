2fce1643ec70b37f7b7b991364afb8cbc5389c96
jeongyeob97 <leejeongyeob97@gmail.com>
2021-04-29 01:13:38 +0900

    홈 뷰 스토리보드 초안 완성

---
+++ Moai_iOS/Moai_iOS/Sources/Cells/HomeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCollectionViewCell.swift
+//  Moai_iOS
+//
+//  Created by 이정엽 on 2021/04/29.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
