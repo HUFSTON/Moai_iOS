a2054070cc06cdf7f714486c8e3c10fe3be0a9ec
jeongyeob97 <leejeongyeob97@gmail.com>
2021-06-06 17:39:53 +0900

    [feat] MyPageUser뷰 구현

---
+++ Moai_iOS/Moai_iOS/Sources/ViewControllers/MyPageSellerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyPageSellerViewController.swift
+//  Moai_iOS
+//
+//  Created by 이정엽 on 2021/06/06.
+//
+
+import UIKit
+
+class MyPageSellerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
