5e28137a2e0e504bba4bb7e29bcdccedc101add6
jeongyeob97 <leejeongyeob97@gmail.com>
2021-06-06 17:56:44 +0900

    [feat] MyPageUser 대략적인 코드 구현

--- Moai_iOS/Moai_iOS/Sources/ViewControllers/MyPageUserViewController.swift
+++ Moai_iOS/Moai_iOS/Sources/ViewControllers/MyPageUserViewController.swift
@@ -9,21 +9,56 @@ import UIKit
 
 class MyPageUserViewController: UIViewController {
 
+    // MARK: - IBoutlets
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    @IBOutlet var pointLabel: UILabel!
+    @IBOutlet var profileEditButton: UIButton!
+    @IBOutlet var chargeButton: UIButton!
+    @IBOutlet var UIViews: [UIView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initializeViews()
+        initializeButtons()
+        initializeGestureRecognizer()
+    }
 
-        // Do any additional setup after loading the view.
+    // MARK: - Private Function
+    private func initializeViews() {
+        UIViews.forEach{
+            $0.layer.borderWidth = 1
+            $0.layer.borderColor = UIColor.LightGray1.cgColor
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func initializeButtons() {
+        self.chargeButton.layer.cornerRadius = 15
+        self.profileEditButton.layer.cornerRadius = 15
+        
     }
-    */
-
+    
+    private func initializeGestureRecognizer() {
+        UIViews[2].addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(touchRegisterView)))
+        UIViews[3].addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(touchTermView)))
+    }
+    
+    // MARK: - objc Function
+    
+    @objc func touchRegisterView() {
+        
+    }
+    
+    @objc func touchTermView() {
+        
+    }
+    
+    // MARK: - IBAction
+    
+    @IBAction func touchProfileEditButton(_ sender: Any) {
+    }
+    
+    @IBAction func touchChargeButton(_ sender: Any) {
+    }
 }
